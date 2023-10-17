2d1927a241c0a6d8f0ca66a35db648c68117ce54
Ermal Mamaj <ermalmamaj@gmail.com>
2023-10-17 21:39:29 +0200

    commit

--- Calculator.py
+++ Calculator.py
@@ -11,39 +11,46 @@ def Devision(a,b):
     result = a / b
     print(f'{a} / {b} = {result}\n')
 
-while True:
+calculate = True
 
-    print("A.Addition")
-    print("B.Subtraction")
-    print("C.Multiplication")
-    print("D.Devision")
-    print("E.Exit")
+while calculate:
+    print("\nSelect operation!\n")
+    print("1.Addition")
+    print("2.Subtraction")
+    print("3.Multiplication")
+    print("4.Devision")
+    print("5.Exit")
     choice= input("Input your choise: ").lower()
 
 
-    if choice == "a" or choice.lower == "addition":
-        print("Addition")
+    if choice == "1" or choice.startswith("a"):
         a = input("First number: ")
         b = input("Second number: ")
         Addition(int(a),int(b))
-    elif choice == "b" or choice.lower == "subtraction":
+    elif choice == "2" or choice.startswith("s"):
         print("Subtraction")
         a = input("First number: ")
         b = input("Second number: ")
         Subtraction(int(a),int(b))
-    if choice == "c" or choice.lower == "multiplication":
+    elif choice == "3" or choice.startswith("m"):
         print("Multiplication")
         a = input("First number: ")
         b = input("Second number: ")
         Multiplication(int(a),int(b))
-    if choice == "d" or choice.lower == "devision":
+    elif choice == "4" or choice.startswith("d"):
         print("Devision")
         a = input("First number: ")
         b = input("Second number: ")
         Devision(int(a),int(b))
-    elif choice == "e" or choice.lower == "exit":
+    elif choice == "5" or choice.startswith("e"):
         print("\nProgram ended")
         quit()
     else:
         print("Couldn't find your choise")
         print("Please try again")
+    calculate = input("Do u want to calculate agin: ").lower()
+    print("\n\n")
+    if calculate.startswith("y"):
+        calculate = True
+    else:
+        calculate = False
